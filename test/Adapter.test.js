9aa37e79937c25939ea7c7f12ae9995373e447d8
wwmin <wwei.min@163.com>
2018-12-02 09:12:32 +0800

    忽略.js

--- test/Adapter.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ava_1 = require("ava");
-const Adapter_1 = require("../src/Adapter");
-ava_1.default("MediaAdapter play", t => {
-    let player = new Adapter_1.AudioPlayer();
-    let playing = player.play("mp3", "aa.mp3");
-    t.true(playing);
-});
